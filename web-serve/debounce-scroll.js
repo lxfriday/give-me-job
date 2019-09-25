92bde2d4f0fff46810eb1f1da37e104ed17980bd
lxfriday <liu3248184446@outlook.com>
2019-09-25 15:32:09 +0800

    feat: 防抖优化连续滚动

---
+++ web-serve/debounce-scroll.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+function debounce(func, wait, immediate) {
+  let timeout
+  function debounced(...args) {
+    // eslint-disable-next-line
+    const ctx = this
+    if (timeout) clearTimeout(timeout)
+
+    if (immediate) {
+      const callNow = !timeout
+      timeout = setTimeout(() => {
+        timeout = null
+      }, wait)
+      if (callNow) func.apply(ctx, args)
+    } else {
+      timeout = setTimeout(() => {
+        func.apply(ctx, args)
+      }, wait)
+    }
+  }
+  debounced.cancel = function cancel() {
+    clearTimeout(timeout)
+    timeout = null
+  }
+  return debounced
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const $page = document.querySelector('#page')
+  function toastNoti(template = '', position = 'top-left') {
+    spop({
+      position,
+      template,
+    })
+  }
+  const debouncedToastNoti = debounce(toastNoti, 500, true)
+  $page.addEventListener('scroll', function(e) {
+    const scrollTop = e.target.scrollTop
+    toastNoti(`滚动到 ${scrollTop}`)
+    debouncedToastNoti(`滚动到 ${scrollTop}`, 'top-right')
+  })
+})
