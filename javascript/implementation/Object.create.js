1e0f2ea0748e4816c716f556639849c38a2fd008
lxfriday <liu3248184446@outlook.com>
2019-12-25 11:12:17 +0800

    fix: object.create

--- javascript/implementation/Object.create.js
+++ javascript/implementation/Object.create.js
@@ -2,14 +2,21 @@
  * Object.create(obj[, propertiesObject])
  * @link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/create
  */
-
-Object.create = function(obj, propertiesObject) {
+Object.create = function(proto, propertiesObject) {
   const res = {}
-  res.__proto__ = obj
+  // proto 只能为 null 或者 type 为 object 的数据类型
+  if (!(proto === null || typeof proto === 'object')) {
+    throw new TypeError('Object prototype may only be an Object or null')
+  }
+  Object.setPrototypeOf(res, proto)
+
+  if (propertiesObject === null) {
+    throw new TypeError('Cannot convert undefined or null to object')
+  }
   if (propertiesObject) {
     Object.defineProperties(res, propertiesObject)
   }
+
   return res
 }
-
 console.log(Object.create(null, 1))
